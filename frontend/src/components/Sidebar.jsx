d78b43ae7c04c3c0d80e17ad9ce18e47edf72724
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-07-27 17:49:22 +0530

    Frontend sidebar added with design

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FaXTwitter } from "react-icons/fa6";
+import { CiHome } from "react-icons/ci";
+import { GoHomeFill } from "react-icons/go";
+import { FaSearch } from "react-icons/fa";
+import { IoMdNotifications } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className='ml-5'>
+        <FaXTwitter size={28}/>
+      </div>
+      <div>
+        <div className='flex items-center space-x-4 p-3 my-1 cursor-pointer hover:bg-neutral-800  transition-colors  duration-200 rounded-full'>
+                <GoHomeFill size="28px"/>
+            <h1 className='font-bold text-lg'>Home</h1>
+        </div>
+        <div className='flex items-center space-x-4 p-3 my-1 cursor-pointer hover:bg-neutral-800  transition-colors  duration-200 rounded-full'>
+                <FaSearch size="28px"/>
+            <h1 className='font-bold text-lg'>Explore</h1>
+        </div>
+        <div className='flex items-center space-x-4 p-3 my-1 cursor-pointer hover:bg-neutral-800  transition-colors  duration-200 rounded-full'>
+                <IoMdNotifications size="28px"/>
+            <h1 className='font-bold text-lg'>Notifications</h1>
+        </div>
+        <div className='flex items-center space-x-4 p-3 my-1 cursor-pointer hover:bg-neutral-800  transition-colors  duration-200 rounded-full'>
+                <FaUser size="28px"/>
+            <h1 className='font-bold text-lg'>Profile</h1>
+        </div>
+        <div className='flex items-center space-x-4 p-3 my-1 cursor-pointer hover:bg-neutral-800  transition-colors  duration-200 rounded-full'>
+                <FaHeart size="28px"/>
+            <h1 className='font-bold text-lg'>Premium</h1>
+        </div>
+        <button className='my-2 px-4 py-3 border-none  text-white font-bold bg-blue-600 w-full rounded-full cursor-pointer text-2xl transition-colors duration-200 hover:bg-blue-700'>Post</button>
+      </div>
+    </div>
+  ) 
+}
+
+export default Sidebar

ad784421e89098a1ee73233b2984976e2d22f009
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-07-30 01:03:32 +0530

    error fixed

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>
-      <div >
-        <img src="https://pbs.twimg.com/profile_images/2699429079/9dd1006a662c15d0387ddf883027bf54_400x400.jpeg" alt="banner" />
-    </div>
     </div>
   )
 }
